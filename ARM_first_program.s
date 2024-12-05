97e9cdaf62b117e6cbfa57520ae19090ad29d582
Mary Kate F. Anecito <139057824+eisBlume0901@users.noreply.github.com>
2024-12-05 16:21:13 +0800

    Update ARM_first_program.s

--- ARM_first_program.s
+++ ARM_first_program.s
@@ -1,5 +1,5 @@
 .global _start
-
+;  The status code 30 is placed in r0, the command 1 (for exit) is placed in r7, and SWI 0 triggers the exit system call.
 ; RUN at https://cpulator.01xz.net/?sys=arm-de1soc remove the comments
 _start:
     MOV r0, #30    ; Load the value 30 into r0 (exit status is 30)
