e328a30c5ee6856ae6847c929f42e660db82c0a6
Mary Kate F. Anecito <139057824+eisBlume0901@users.noreply.github.com>
2024-12-05 16:09:36 +0800

    Update ARM_first_program.s

--- ARM_first_program.s
+++ ARM_first_program.s
@@ -2,6 +2,6 @@
 
 ; RUN at https://cpulator.01xz.net/?sys=arm-de1soc remove the comments
 _start:
-    MOV r0, #30    ; Load the value 30 into r0 (exit status)
-    MOV r7, #1     ; Set r7 to 1 (exit system call number in ARM Linux)
+    MOV r0, #30    ; Load the value 30 into r0 (exit status is 30)
+    MOV r7, #1     ; Set r7 to 1 (exit system call number is 1in ARM Linux)
     SWI 0          ; Trigger the software interrupt (exit the program)
