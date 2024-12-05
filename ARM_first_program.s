e59f867c745ab0cc7a898a4541ffad0a93c17d8c
Mary Kate F. Anecito <139057824+eisBlume0901@users.noreply.github.com>
2024-12-05 16:06:02 +0800

    Update ARM_first_program.s

--- ARM_first_program.s
+++ ARM_first_program.s
@@ -2,7 +2,7 @@
 
 _start:
     ; NOTE remove comments to execute https://cpulator.01xz.net/?sys=arm-de1soc
-    ; MOV means transferring data
+    ; MOV is instructing r0 register to have a value of 30
     MOV r0, #30    ; Load the value 30 into r0 (exit status)
     MOV r7, #1     ; Set r7 to 1 (exit system call number in Linux ARM)
     SWI 0          ; Trigger the software interrupt (exit)
